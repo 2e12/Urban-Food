eb230e754c46efc80d1bd2ec7bfe5c7fd3a7be23
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-01 10:54:25 +0100

    Added js file and JQuery. Added menuToggle function

--- public/index.php
+++ public/index.php
@@ -6,13 +6,15 @@
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Condensed:300,400,500|IBM+Plex+Sans:300,400,500" rel="stylesheet">
     <meta name="theme-color" content="#57d319">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+    <script src="js/main.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 </head>
 <body>
 <div id="header">
     <div class="wrapper">
         <div id="navigation">
-            <div class="menubutton">
+            <div class="menubutton" onclick="toggleMenu()">
                 <div></div>
                 <div></div>
                 <div></div>
