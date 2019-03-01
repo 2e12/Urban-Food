3c6086085edb9020a5906a6b0e662d9bc5a39c5d
bzehni <ivan.zehnder@bbcag.ch>
2019-03-01 11:29:33 +0100

    Dispatcher Commit

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@
     //Content start
     Dispatcher::dispatch();
     require "../template/test_content.php";
-    
+
     //Content end
     require "../template/nav.php";
     require "../template/footer.php";
