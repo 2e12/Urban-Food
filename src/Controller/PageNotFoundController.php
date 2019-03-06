cf1dbbf465a54e5f91d0d1a85416c25a71a0bce7
bzehni <ivan.zehnder@bbcag.ch>
2019-03-06 07:52:15 +0100

    Structure Commit

--- src/Controller/PageNotFoundController.php
+++ src/Controller/PageNotFoundController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 class PageNotFoundController
 {
     public function index() {
-        echo "<h1>Error 404</h1><br><h2>Page not found</h2><br><img src='../../public/img/PageNotFoundSmiley.JPG'>";
+        $view = new View('pagenotfound/index');
+        $view->content = "<h1>Error 404</h1><br><h2>Page not found</h2><br><img src='../../public/img/PageNotFoundSmiley.JPG' alt='error404'>";
+        $view->display();
     }
 }
