d5d0ac8488a542ca492e9dd55d3f4eb13d7a24b7
bzehni <ivan.zehnder@bbcag.ch>
2019-03-07 09:46:47 +0100

    Styled up login area

--- template/Login/index.php
+++ template/Login/index.php
@@ -2,9 +2,20 @@
 echo
 '<div class="content">
     <h1>Login</h1>
-    <form method="post" action="">
-        <input name="emailadress" placeholder="deine Email-Adresse">
-        <input name="password" placeholder="dein Passwort">
-        <button type="submit">einloggen</button>
-    </form>    
+    <div class="formlayout">
+         <div class="formblock">
+            <form method="post" action="">
+                <input name="emailadress" placeholder="deine Email-Adresse">
+                <input name="password" placeholder="dein Passwort">
+                <button type="submit">einloggen</button>
+            </form>
+        </div>
+        <div class="imageblock">
+            <div class="images">
+                <div class="TwoSpan"><img src="/img/upload/asia/asia.jpg" alt="asia food"></div>
+                <div class="OneSpan"><img src="/img/upload/burger/burger.jpg" alt="burgers"></div>
+                <div class="ThreeSpan"><img src="/img/upload/pizza/pizza.jpg" alt="pizzas"></div>
+            </div>  
+        </div>
+    </div>
 </div>';
