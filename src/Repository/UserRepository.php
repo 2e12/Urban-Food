249f3075a65a24e11239b4d6da6df04bc15eb395
bzehni <ivan.zehnder@bbcag.ch>
2019-03-07 16:07:32 +0100

    Registry Essential Commit

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,11 +8,12 @@ class UserRepository extends Repository
 {
     protected $tablename = 'user';
 
-    public function readByEmail(string $email): object {
+    public function readByEmail(string $email)
+    {
         $query = "SELECT * FROM {$this->tableName} WHERE email=?";
 
         $statement = ConnectionHandler::getConnection()->prepare($query);
-        $statement->bind_param('i', $email);
+        $statement->bind_param('s', $email);
 
         $statement->execute();
 
@@ -26,5 +27,31 @@ class UserRepository extends Repository
         $result->close();
 
         return $row;
+    }
+
+    public function insert(string $city, string $postalcode, string $street, string $email, string $firstname, string $lastname, string $password)
+    {
+        $adressQuery = "INSERT INTO adress (city, postal_code, street) VALUES (?, ?, ?)";
+        $adressSelectQuery = "SELECT id FROM adress WHERE city = ? AND postal_code = ? AND street = ?";
+
+        $adressStatement = ConnectionHandler::getConnection()->prepare($adressQuery);
+        $adressStatement->bind_param('sss', $city, $postalcode, $street);
+        $adressStatement->execute();
+
+        $adressSelectStatement = ConnectionHandler::getConnection()->prepare($adressSelectQuery);
+        $adressSelectStatement->bind_param('sss', $city, $postalcode, $street);
+        $adressSelectStatement->execute();
+        $result = $adressSelectStatement->get_result();
+        if (!$result) {
+            throw new Exception($adressSelectStatement->error);
+        }
+        $row = $result->fetch_object();
+        $result->close();
+
+        $userQuery = "INSERT INTO {$this->tableName} (prename, lastname, password, email, adress_id, is_admin) VALUES (?, ?, ?, ?, ?, ?)";
+
+        $userStatement = ConnectionHandler::getConnection()->prepare($userQuery);
+        $userStatement->bind_param('ssssii', $firstname, $lastname, hash('sha256', $password), $email, $row->id, 0);
+        $userStatement->execute();
     }
 }
