957b62cf11855c50e7f91aee63eba69582347a54
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-08 07:35:02 +0100

    Product link is now pointing to "category/"

--- template/nav.php
+++ template/nav.php
@@ -2,7 +2,7 @@
     <h2>Menu</h2>
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="/product">Products</a></li>
+        <li><a href="/category">Products</a></li>
         <li><a href="/login">Login</a></li>
         <li><a href="/register">Register</a></li>
     </ul>
