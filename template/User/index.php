8af2f374254f4a21115d095bc7f0cce5bf7666e3
bzehni <ivan.zehnder@bbcag.ch>
2019-03-13 12:28:28 +0100

    Added safe areas

--- template/User/index.php
+++ template/User/index.php
@@ -1,7 +1,19 @@
 <?php
 $firstname = \App\Authentication\Authentication::getAuthenticatedUser()->prename;
+$permissions = \App\Authentication\Authentication::getAuthenticatedUser()->is_admin;
+
 echo  '<div class="content">
       <h1>Hallo '. $firstname .'</h1><br>
       <p>Was möchtest du tun?</p><br>
-      <a href="/User/logout">abmelden</a><br>
-      <div class="adminarea"><button>hallowelt</button></div></div>';
+      <a class="linkbutton" href="/User/logout">abmelden</a><br>';
+
+if ($permissions == true) {
+    echo '<a class="linkbutton" href="/User/create">neue User erfassen</a><br>
+          <a class="linkbutton" href="/User/grant">Berechtigungen bearbeiten</a><br>
+          <a class="linkbutton" href="/Product/create">neues Produkt erfassen</a>';
+}
+else {
+    echo '<a class="linkbutton" href="/category">weiter zum Shop</a>';
+}
+
+echo '</div>';
