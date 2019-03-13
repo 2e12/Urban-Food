c278d31b9d30227c6a454891af1a3b05e720aa71
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-13 15:24:49 +0100

    Added Order Controller.
    Get's shoppingcart array form client and checks if every requierd value exist and safes the array in the "order" session

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Authentication\Authentication;
+use App\View\View;
+
+class OrderController
+{
+    public function index(): void
+    {
+
+    }
+
+    public function buy(): void
+    {
+    }
+
+    public function checkout(): void
+    {
+        if (isset($_SESSION["order"])) {
+
+            $view = new View('Order/checkout');
+
+            $view->title = 'Checkout';
+            $view->display();
+
+        }
+    }
+
+    public function check(): void
+    {
+        $valid = false;
+        $products = array();
+        //JavaScript sends a Json with products form the shoppingcart
+        if (isset($_POST["products"])) {
+            $request_data = $_POST["products"];
+            if (is_array($request_data)) {
+                $valid = true;
+                foreach ($request_data as $item) {
+                    //Validating. Correct stuff will be filled in a new array.
+                    if (isset($item["id"]) and isset($item["quantity"])) {
+                        if (ctype_digit($item["id"]) and ctype_digit($item["quantity"])) {
+                            $products[] = array("id" => $item["id"], "quantity" => $item["quantity"]);
+                        }
+                        else {
+                            $valid = false;
+                        }
+                    }
+                    else {
+                        $valid = false;
+                    }
+                }
+            }
+        }
+        if (!$valid) {
+            exit;
+        }
+        echo "ok";
+        $_SESSION["order"] = $products;
+    }
+}
