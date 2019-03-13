1e992a6b45c39ad18ba4bcb06c3df12d8aee74be
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-13 15:35:51 +0100

    Checking items in DB for Shoppingcart

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Authentication\Authentication;
+use App\Repository\ProductRepository;
 use App\View\View;
 
 class OrderController
@@ -21,7 +22,6 @@ class OrderController
         if (isset($_SESSION["order"])) {
 
             $view = new View('Order/checkout');
-
             $view->title = 'Checkout';
             $view->display();
 
@@ -37,11 +37,16 @@ class OrderController
             $request_data = $_POST["products"];
             if (is_array($request_data)) {
                 $valid = true;
+                $repository = new ProductRepository();
                 foreach ($request_data as $item) {
                     //Validating. Correct stuff will be filled in a new array.
                     if (isset($item["id"]) and isset($item["quantity"])) {
                         if (ctype_digit($item["id"]) and ctype_digit($item["quantity"])) {
-                            $products[] = array("id" => $item["id"], "quantity" => $item["quantity"]);
+                            $product = $repository->readById($item["id"]);
+                            if ($product !== null) {
+                                $product->quantity = $item["quantity"];
+                                $products[] = array($product);
+                            }
                         }
                         else {
                             $valid = false;
