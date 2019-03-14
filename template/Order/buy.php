cfc27ab3d7f3f9e6d12a6d4c3edc8fb5038252f2
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-14 09:17:33 +0100

    Added buy.php in view.

---
+++ template/Order/buy.php
@@ -0,0 +1,5 @@
+<div class="content">
+    <h1>Vielen Dank</h1>
+    <p>Vielen Dank für Ihre Bestellung. Ihre Lieferung ist bereits auf dem Weg. Im Benutzer Profil können Sie weitere
+        Details über Ihre Bestellung einsehen.</p>
+</div>
