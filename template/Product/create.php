74a094a37506a0e481b286d8db03ca1b4e89a28b
bzehni <ivan.zehnder@bbcag.ch>
2019-03-14 08:26:10 +0100

    Added security to safe areas

--- template/Product/create.php
+++ template/Product/create.php
@@ -1,21 +1,24 @@
 <?php
-echo '
-<div class="content">
-    <h1>Neues Produkt erfassen</h1>
-    <div class="formblock">
-       <form method="POST" action="/Product/createProduct" class="smallform">
-        <input name="productName" type="text" placeholder="Name des Produkts" required>
-        <input name="productPrice" type="number" step="0.01" placeholder="Preis" required>
-        <input name="productDesc" type="text" placeholder="Beschreibung des Produkts" required>
-        <input name="productPath" type="text" placeholder="Dateiname des Bilds" required>
-        <select name="productCategory" required>
-            <option value="0">Sandwich</option>
-            <option value="1">burger</option>
-            <option value="2">snacks</option>
-            <option value="3">drinks</option>
-            <option value="4">asia</option>
-        </select>
-        <div class="send"><input type="submit"></div>
-        </form>
-    </div>
-</div>';
+$permission = \App\Authentication\Authentication::getAuthenticatedUser()->is_admin;
+if ($permission == true) {
+    echo '
+    <div class="content">
+        <h1>Neues Produkt erfassen</h1>
+        <div class="formblock">
+           <form method="POST" action="/Product/createProduct" class="smallform">
+            <input name="productName" type="text" placeholder="Name des Produkts" required>
+            <input name="productPrice" type="number" step="0.01" placeholder="Preis" required>
+            <input name="productDesc" type="text" placeholder="Beschreibung des Produkts" required>
+            <input name="productPath" type="text" placeholder="Dateiname des Bilds" required>
+            <select name="productCategory" required>
+                <option value="0">Sandwich</option>
+                <option value="1">burger</option>
+                <option value="2">snacks</option>
+                <option value="3">drinks</option>
+                <option value="4">asia</option>
+            </select>
+            <div class="send"><input type="submit"></div>
+            </form>
+        </div>
+    </div>';
+}
--- template/Product/delete.php
+++ template/Product/delete.php
@@ -1,25 +1,28 @@
 <?php
 $repo = new \App\Repository\ProductRepository();
 $products = $repo->readAll();
-echo '
-<div class="content">
-    <div class="del">
-    <h1>Produkte löschen</h1>
-    <table>
-        <tr>
-            <td>Produkt</td>
-            <td>Preis</td>
-        </tr>';
+$permission = \App\Authentication\Authentication::getAuthenticatedUser()->is_admin;
+if ($permission == true) {
+    echo '
+    <div class="content">
+        <div class="del">
+        <h1>Produkte löschen</h1>
+        <table>
+            <tr>
+                <td>Produkt</td>
+                <td>Preis</td>
+            </tr>';
         foreach ($products as $product) {
             echo '
-                <tr>
-                    <td>'.$product->name.'</td>
-                    <td>'.$product->price.'</td>
-                    <td><a href="/Product/del?id='.$product->id.'"><i class="fas fa-ban"></i></a></td>
-                </tr>
-            ';
+                    <tr>
+                        <td>'.$product->name.'</td>
+                        <td>'.$product->price.'</td>
+                        <td><a href="/Product/del?id='.$product->id.'"><i class="fas fa-ban"></i></a></td>
+                    </tr>
+                ';
         }
-    echo '    
-    </table>
-    </div>
-</div>';
+        echo '    
+        </table>
+        </div>
+    </div>';
+}
