7091a1f6d98feb0ddca992620b053d3ee66050af
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-14 12:55:36 +0100

    buy gives now the order id to the view in order to make a link to the open created order.

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Authentication\Authentication;
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\View\View;
 
@@ -15,23 +16,67 @@ class OrderController
 
     public function buy(): void
     {
-        if (isset($_POST["buy"]) and isset($_POST["comment"])) {
-            $view = new View('Order/buy');
-            $view->title = 'Danke für Ihren Einkauf';
-            $view->display();
+        if (isset($_SESSION["user"]) and isset($_SESSION["order"])) {
+            //POST "Buy" is the submit button
+            if (isset($_POST["buy"]) and isset($_POST["comment"]) and isset($_POST["allergy"])) {
+                $comment = htmlspecialchars(substr($_POST["comment"], 0, 500));
+                $allergy = htmlspecialchars(substr($_POST["allergy"], 0, 30));
+
+                $repository = new OrderRepository();
+                $id = $repository->insertOrder($_SESSION["user"], $comment, $allergy, $_SESSION["order"]);
+
+                $view = new View('Order/buy');
+                $view->title = 'Danke für Ihren Einkauf';
+                $view->orderId = $id;
+                $view->display();
+                unset($_SESSION["order"]);
+            }
+            else {
+                header("location: /");
+                exit;
+            }
+        }
+        else {
+            header("location: /");
+            exit;
         }
     }
 
     public function checkout(): void
     {
         if (isset($_SESSION["order"])) {
-
             $view = new View('Order/checkout');
             $view->title = 'Checkout';
             $view->basket = $_SESSION["order"];
             $view->display();
-
         }
+    }
+
+    public function show(): void
+    {
+        $authenticated = false; //This Boolean indicates if an user can access an order.
+        $order = null;
+        $products = null;
+        if (isset($_GET["id"]) and isset($_SESSION["user"])) {
+            $repository = new OrderRepository();
+            $order = $repository->readById($_GET["id"]);
+            $products = $repository->readProductsByOrderId($_GET["id"]);
+            if ($order and $products) {
+                if ($order->user_id == $_SESSION["user"]) {
+                    $authenticated = true;
+                }
+            }
+        }
+        if (!$authenticated) { //Go to start page in case user isn't allowed to see the order.
+            header("location: /");
+            exit;
+        }
+        $view = new View('Order/show');
+        $view->title = 'Einkauf';
+        $view->comment = $order->comment;
+        $view->allergy = $order->allergy;
+        $view->products = $products;
+        $view->display();
     }
 
     public function check(): void
