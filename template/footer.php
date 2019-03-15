cd4fa8cd04fba65b339b19a7eecfafc06558933d
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-15 11:24:47 +0100

    Added Footer

--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,28 @@
-
+<div id="footer">
+    <p>© Urban Food 2019</p>
+    <div class="row">
+        <strong>About us</strong><br>
+        <a href="/static/team">Team</a><br>
+        <a href="/static/impressum">Impressum</a><br>
+    </div>
+    <div class="row">
+        <strong>Store</strong><br>
+        <a href="/">Home</a><br>
+        <a href="/Category">Products</a><br>
+        <?php
+        if (isset($_SESSION["user"])) {
+            ?>
+            <a href="/User">My Profile</a><br>
+            <?php
+        }
+        else {
+            ?>
+            <a href="/register">Register</a><br>
+            <a href="/login">Login</a><br>
+            <?php
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
