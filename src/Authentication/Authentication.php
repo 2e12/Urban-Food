7e2a2460f219883fa9986caf4bfd173df141074b
bzehni <ivan.zehnder@bbcag.ch>
2019-03-21 10:33:00 +0100

    Fully commented the file

--- src/Authentication/Authentication.php
+++ src/Authentication/Authentication.php
@@ -7,6 +7,13 @@ use RuntimeException;
 
 class Authentication
 {
+    /**
+     * Diese Funktion überprüft ob die vom User eingegebenen Daten auch in der Datenbank
+     * vorhanden sind. Falls ja, schreibt sie auch die UserId in den SESSION-Array.
+     * @param $email Die vom User eingegebene Email-Adresse
+     * @param $password Das vom User mitgegebene Passwort
+     * @return bool Der Wert der aussagt, ob das Login erfolgreich war
+     */
     public static function login($email, $password): bool
     {
         // Den Benutzer anhand der E-Mail oder des Benutzernamen auslesen
@@ -28,12 +35,27 @@ class Authentication
         return false;
     }
 
+    /**
+     * Diese Funktion legt anhand der Daten des Users einen neuen Benutzer in der Datenbank an.
+     * @param string $textCity Die Stadt in der der User wohnt
+     * @param string $textPostalcode Die Postleitzahl der eben erwähnten Stadt
+     * @param string $textStreet Die Strasse in der der User lebt
+     * @param string $textEmail Die Email-Adresse des Users
+     * @param string $textFirstname Vorname des Users
+     * @param string $textLastname Nachname des Users
+     * @param string $textPassword Das vom User gewählte Passwort
+     * @param bool $textFast Ein Wert der aussagt, ob ein normaler User den Account anlegt, oder ein Admin viele Accounts anlegen will
+     */
     public static function register(string $textCity, string $textPostalcode, string $textStreet, string $textEmail, string $textFirstname, string $textLastname, string $textPassword, bool $textFast)
     {
         $users = new UserRepository();
         $users->insert($textCity, $textPostalcode, $textStreet, $textEmail, $textFirstname, $textLastname, $textPassword, $textFast);
     }
 
+    /**
+     * Mit dieser Funktion kann ein User sich ausloggen und damit die Session
+     * zurücksetzen und anschliessend löschen.
+     */
     public static function logout(): void
     {
         // TODO: Mit unset die Session-Werte löschen
@@ -43,6 +65,11 @@ class Authentication
         session_destroy();
     }
 
+    /**
+     * Diese Funktion prüft ob der aktive User in der Datenbank als Administrator erfasst ist.
+     * @param $textEmail Die Email-Adresse des aktiven Users
+     * @return bool Der Wert sagt aus, ob der User nun ein Admin ist oder nicht
+     */
     public static function isAdmin($textEmail): bool {
         $users = new UserRepository();
         $user = $users->readByEmail($textEmail);
@@ -59,6 +86,10 @@ class Authentication
         }
     }
 
+    /**
+     * Überprüft, ob in Session momentan eine UserId gespeichert ist.
+     * @return bool Sagt aus ob nun eine UserId gesetzt ist oder nicht
+     */
     public static function isAuthenticated(): bool
     {
         // TODO: Zurückgeben ob eine ID in der Session gespeichert wurde (true/false)
@@ -70,6 +101,11 @@ class Authentication
         }
     }
 
+    /**
+     * Gibt dem eingeloggten User aus der Datenbank zurück.
+     * @return \App\Repository\Der Das User-Objekt aus der Datenbank
+     * @throws \Exception Eine mögliche Exception von der Datenbank-Verbindung
+     */
     public static function getAuthenticatedUser()
     {
         // TODO: User anhand der ID aus der Session auslesen
@@ -78,14 +114,5 @@ class Authentication
 
         // TODO: User zurückgeben
         return $user;
-    }
-
-    public static function restrictAuthenticated(): void
-    {
-        if (!self::isAuthenticated()) {
-            throw new RuntimeException("Sie haben keine Berechtigung diese Seite anzuzeigen.");
-            // TODO: Loggen! Unbefungte Zugriffsversuche sollten geloggt werden
-            exit();
-        }
     }
 }
