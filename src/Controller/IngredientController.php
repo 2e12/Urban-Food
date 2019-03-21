5c881b8e057fa2dfb040aa1ef7573ad21a413db7
bzehni <ivan.zehnder@bbcag.ch>
2019-03-21 10:43:51 +0100

    Fully commented IngredientController.php

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -7,12 +7,18 @@ use App\View\View;
 
 class IngredientController
 {
+    /**
+     * Diese Funktion erstellt die View für das Erstellen einer neuen Zutat.
+     */
     public function create(): void {
         $view = new View('Ingredient/create');
         $view->title = 'Create new ingredient';
         $view->display();
     }
 
+    /**
+     * Die Funktion erstellt die Zutat in der Datenbank
+     */
     public function createIngredient(): void {
       if (isset($_POST["ingredientName"])) {
           $repo = new IngredientRepository();
@@ -21,12 +27,18 @@ class IngredientController
       }
     }
 
+    /**
+     * Diese Funktion erstellt die View für das Löschen einer Zutat.
+     */
     public function delete(): void {
         $view = new View('Ingredient/delete');
         $view->title = 'Delete ingredient';
         $view->display();
     }
 
+    /**
+     * Die Funktion löscht die Zutat in der Datenbank
+     */
     public function del(): void {
         if (isset($_GET['name'])) {
             $repo = new IngredientRepository();
