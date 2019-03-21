386648c5fbb5c2a69aa3fbec040520e9f7726eae
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-21 08:55:56 +0100

    Loading category background from DB

--- template/Category/index.php
+++ template/Category/index.php
@@ -3,7 +3,7 @@
     <?php
     foreach ($categories as $category) {
         ?>
-        <a data-background="https://foodrevolution.org/wp-content/uploads/2018/04/blog-featured-diabetes-20180406-1330.jpg"
+        <a data-background="<?php echo $category->image_path ?>"
            href="/category/products/?category=<?php echo $category->id ?>" class="category"><h2>
                 <?php
                 echo $category->name;
