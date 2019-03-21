0443ae0f7accbf8e70f7b924198a1a841db4f27a
bzehni <ivan.zehnder@bbcag.ch>
2019-03-21 13:05:46 +0100

    Fully commented Product_IngredientRepository.php

--- src/Repository/Product_IngredientRepository.php
+++ src/Repository/Product_IngredientRepository.php
@@ -8,6 +8,11 @@ class Product_IngredientRepository extends Repository
 {
     protected $tableName = 'product_ingredient';
 
+    /**
+     * Baut Verbindung zur Datenbank auf und gibt Verbindungen anhand der ProductId zurück.
+     * @param int $productId Die Id des Produktes
+     * @return array Die Verbindungen zu Zutaten die das gewählte Produkt aufweist
+     */
     public function readByProductId(int $productId): array
     {
         $query = "SELECT * FROM {$this->tableName} JOIN ingredient ON ingredient_id = ingredient.id WHERE product_id = ? LIMIT 0, 100";
@@ -29,6 +34,11 @@ class Product_IngredientRepository extends Repository
         return $rows;
     }
 
+    /**
+     * Baut Verbindung zur Datenbank auf und fügt eine neue Verbindung zwischen einem Produkt und einer Zutat ein.
+     * @param int $prodId Die Id des zu verbindenden Produkts
+     * @param int $ingId Die Id der zu verbindenden Zutat
+     */
     public function insert(int $prodId, int $ingId): void {
         $insertQuery = "INSERT INTO {$this->tableName} (product_id,ingredient_id) VALUES (?,?)";
         $insertStatement = ConnectionHandler::getConnection()->prepare($insertQuery);
