650715cc04d1d1f16830eeb29cbdd03088bb2f4d
bzehni <ivan.zehnder@bbcag.ch>
2019-03-21 11:19:49 +0100

    Fully commented UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,9 @@ use App\View\View;
 
 class UserController
 {
+    /**
+     * Überprüft ob der User eingeloggt ist und wenn ja, wird ein persönlicher Bereich geladen.
+     */
     public function index(): void {
 
         if (isset($_SESSION['user'])) {
@@ -22,18 +25,28 @@ class UserController
         }
     }
 
+    /**
+     * Erstellt das Viewfile um den Usern Admin-Rechte zu gewähren.
+     */
     public function grant(): void {
         $view = new View('User/grant');
         $view->title = 'Grant';
         $view->display();
     }
 
+    /**
+     * Erstellt das Viewfile um neue User zu erstellen.
+     */
     public function create(): void {
         $view = new View('User/create');
         $view->title = 'Create';
         $view->display();
     }
 
+    /**
+     * Ändert die Berechtigung des Users in der Datenbank.
+     * @throws \Exception Exception in der MySQLi-Verbindung
+     */
     public function changePermissions(): void {
         if (isset($_POST['useremail']) && isset($_POST['newPerm']) && filter_var($_POST['useremail'], FILTER_VALIDATE_EMAIL)) {
             $users = new UserRepository();
@@ -49,6 +62,9 @@ class UserController
         header('Location: /User/grant');
     }
 
+    /**
+     * Erstellt das Viewfile um dem User mitzuteilen, dass er sich ausgeloggt hat.
+     */
     public function logout(): void {
         Authentication::logout();
         $view = new View('User/logout');
@@ -56,12 +72,18 @@ class UserController
         $view->display();
     }
 
+    /**
+     * Erstellt das Viewfile um mitzuteilen, dass die Login-Informationen falsch sind.
+     */
     public function wronginformations() {
         $view = new View('user/wronginformations');
         $view->title = 'Wrong Informations';
         $view->display();
     }
 
+    /**
+     * Erstellt das Viewfile um zu zeigen, dass der User kein Zugriff auf diesen Bereich hat.
+     */
     public function forbidden(): void {
         $view = new View('/User/forbidden');
         $view->title = 'Forbidden!';
