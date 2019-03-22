6ca3060e47d73c35f9d68be0d1f57650256e7953
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-22 08:20:00 +0100

    Removed comment

--- template/Default/index.php
+++ template/Default/index.php
@@ -31,10 +31,4 @@
             <div class="top">Drinks</div>
         </div>
     </a>
-
-
-    <!--<div class="row"><div class="product burger" onclick="location.href='/category/products/?category=1'"><div class="top">Burger</div></div><div class="linkbutton right"><a href="/category/products/?category=1">Zur Kategorie</a></div></div><br>
-    <div class="row"><div class="linkbutton left"><a href="/category/products/?category=5">Zur Kategorie</a></div><div class="product pizza" onclick="location.href='/category/products/?category=5'"><div class="top">Pizza</div></div></div><br>
-    <div class="row"><div class="product softdrinks" onclick="location.href='/category/products/?category=3'"><div class="top">Softdrinks</div></div><div class="linkbutton right"><a href="/category/products/?category=3">Zur Kategorie</a></div></div>
-    -->
 </div>
