ac7984732e4b7c362c2b3908d5b8de4a9a7ff708
bbeutg <gabriel.beutler@bbcag.ch>
2019-03-27 13:56:29 +0100

    Changed DB to Server

--- src/config.php
+++ src/config.php
@@ -1,2 +1,2 @@
 <?php
-return array("database" => array("host" => "192.168.52.18", "username" => "urban_food", "password" => "sml12345", "database" => "urbanfood"));
+return array("database" => array("host" => "localhost", "username" => "bbeutgsql1", "password" => "mjk4QE6a", "database" => "bbeutgsql1"));
